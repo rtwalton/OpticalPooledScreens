f90aaacbc42000689ba881b2fd276c271c7a6691
lukebfunk <lfunkonline@gmail.com>
2021-09-03 12:27:23 -0400

    tqdm imports

--- ops/imports_ipython.py
+++ ops/imports_ipython.py
@@ -12,8 +12,7 @@ IPython.get_ipython().run_line_magic('autoreload', '2')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import tqdm.notebook
-tqdn = tqdm.notebook.tqdm
+from tqdm.auto import tqdm
 
 from matplotlib.colors import ListedColormap
 GLASBEY_PLT = ListedColormap((GLASBEY.reshape(3,256).T)/256)
