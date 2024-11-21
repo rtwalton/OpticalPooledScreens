dba0037467ace4cd8ce53c479892a537662190c7
Russell Walton <61611503+rtwalton@users.noreply.github.com>
2024-11-21 14:35:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 # Create a new virtual environment called 'venv_ops'
-python3 -m venv venv_ops
+#python3 -m venv venv_ops
 
 # Activate the 'venv_ops' virtual environment
-. venv_ops/bin/activate
+#. venv_ops/bin/activate
 
 # Install wheel package to support binary installations
 pip install wheel
 
 # Install dependencies from the requirements file
+pip install numpy==1.23.5
 pip install -r requirements_ops.txt
 
 # Link ops package instead of copying
@@ -17,6 +18,6 @@ pip install -r requirements_ops.txt
 pip install -e .
 
 # Register the 'venv_ops' virtual environment with Jupyter
-python -m ipykernel install --user --name=venv_ops
+# python -m ipykernel install --user --name=venv_ops
 
-echo "Installation complete. Virtual environment 'venv_ops' is set up."
+echo "Installation complete."
